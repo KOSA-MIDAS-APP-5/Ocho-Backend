b2e000ff34c5cc3977e3ce96ca191fc09cdcea18
nswon <swnam0729@gmail.com>
2022-11-04 06:32:04 +0900

    docs: 프로젝트 설명 자료 작성

--- src/main/java/hacathon/hacathon/domain/user/util/AdminUtil.java
+++ src/main/java/hacathon/hacathon/domain/user/util/AdminUtil.java
@@ -3,6 +3,6 @@ package hacathon.hacathon.domain.user.util;
 public class AdminUtil {
     private AdminUtil() {}
 
-    public static final String ADMIN_NAME = "admin";
-    public static final String ADMIN_PASSWORD = "1234";
+    public static final String ADMIN_NAME = "Admin";
+    public static final String ADMIN_PASSWORD = "12345";
 }
