bd2f3c23ccee4887d0604bdb1080fac7b498193f
nswon <swnam0729@gmail.com>
2022-11-03 10:26:51 +0900

    init: 프로젝트 빌드

---
+++ src/main/java/hacathon/hacathon/HacathonApplication.java
@@ -0,0 +1,13 @@
+package hacathon.hacathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HacathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HacathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hacathon/hacathon/HacathonApplicationTests.java
@@ -0,0 +1,13 @@
+package hacathon.hacathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HacathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
