a586fe13811071022cba67ee3d6c67e3458b2d40
nswon <swnam0729@gmail.com>
2022-11-04 07:13:51 +0900

    feat: 배포를 위한 테스트 주석처리

--- src/test/java/hacathon/hacathon/HacathonApplicationTests.java
+++ src/test/java/hacathon/hacathon/HacathonApplicationTests.java
@@ -3,11 +3,11 @@ package hacathon.hacathon;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class HacathonApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
+//@SpringBootTest
+//class HacathonApplicationTests {
+//
+//	@Test
+//	void contextLoads() {
+//	}
+//
+//}
